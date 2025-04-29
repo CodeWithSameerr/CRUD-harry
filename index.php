f5f42b806113f226f8ac1466a8d831bbf3444cd2
CodeWithSameerr <prvsameer@gmail.com>
2025-04-29 16:19:16 +0500

    file converted into php

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php 
+
+// INSERT INTO `note` (`sno`, `title`, `description`, `tstamp`) VALUES (NULL, 'yawar refund my money', 'Hey Yawar,\r\n\r\nPlease bring my money back as u say so u will return my money on 27 august!', current_timestamp());
+
+// DB Connection 
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'notes';
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+if(!$conn){
+
+    die("Sorry Database not Connected". mysqli_connect_error());
+
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>iNotes - Notes taking made easy</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container">
+              <a class="navbar-brand" href="#">iNotes</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Services</a>
+                  </li>
+            
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                  </li>
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+    </header>
+    <main>
+        <div class="container my-3">
+            <h2>Add a Note</h2>
+            <form>
+                <div class="mb-3">
+                    <label for="title" class="form-label">Note Title</label>
+                    <input type="text" class="form-control" id="title" name="title" >
+                  </div>
+
+                  <div class="mb-3">
+                    <label for="desc" class="form-label">Note Description</label>
+                    <textarea class="form-control"  id="desc" name="desc" rows="3"></textarea>
+                  </div>
+                <button type="submit" class="btn btn-primary">Add Note</button>
+              </form>
+        </div>
+
+        <div class="container">
+            <?php 
+
+              $sql = "SELECT * FROM `note`";
+              $result = mysqli_query($conn , $sql);
+              
+                while ($row = mysqli_fetch_assoc($result)) {
+                  echo $row['sno']. "<br> Title: ". $row['title']. "<br> Desc is: ". $row['description'] 
+                  . "<br>";
+                }
+            ?>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
